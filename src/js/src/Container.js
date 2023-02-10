0c263a81c505183312326fc598b1aa3cdd1d31bb
Ghita Florin <ghitafm94@gmail.com>
2023-02-10 16:38:16 +0200

    Added Postgres

--- src/js/src/Container.js
+++ src/js/src/Container.js
@@ -1,6 +1,6 @@
 import React from "react";
 const Container = props => (
-    <div style = {{width : '1400px', margin: '0 auto'}}>
+    <div style = {{width : '1400px', margin: '0 auto', textAlign: 'center'}}>
         {props.children}
     </div>
 );
